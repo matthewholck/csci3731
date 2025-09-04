e9f01fb7dca26d8a7f1456750a30ed4fae2cfbde
matthewholck <143768909+matthewholck@users.noreply.github.com>
2025-09-03 15:48:40 -1000

    Create hw01.cc

---
+++ homework1/hw01.cc
@@ -0,0 +1,20 @@
+#include <cstdio>
+
+int getX (int x, int y, int a){
+    return (x*x) - (y*y) + a;
+}
+
+int getY (int x, int y, int b){
+    return 2*x*y + b;
+}
+
+int main(int argc, char** argv) {
+    
+    int x=0, y=0;
+    float a=0.1, b=0.2
+
+    for(i=0; i<10000000; i++){
+        try
+    }
+    
+}
