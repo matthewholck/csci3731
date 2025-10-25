fe772280277993d79a3c968cd54623e8cad270e0
matthewholck <143768909+matthewholck@users.noreply.github.com>
2025-10-25 09:57:59 -1000

    Delete homework06 directory

--- homework06/ppm_handler.h
+++
@@ -1,8 +0,0 @@
-#ifndef PPM_HANDLER
-#define PPM_HANDLER
-
-void readPPM (char* magic, int* x, int* y, int* pixel, unsigned char** array);
-void writePPM (const char *filename, int x, int y, int pixel, const unsigned char *data);
-void toJPEG (const char *filename, unsigned char *data, int x, int y, int quality);
-
-#endif
