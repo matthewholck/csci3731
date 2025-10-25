ea5b7ffcf936669f4552347500ea7c5f7c3cb5eb
matthewholck <143768909+matthewholck@users.noreply.github.com>
2025-10-25 09:19:19 -1000

    add 7.1 portion

--- homework06/hw06.cc
+++ homework06/hw06.cc
@@ -1,1 +1,20 @@
+#include <cstdio>
+#include <jpeglib.h>
+#include <cstdlib>
+#include "ppm_handler.h" 
 
+int main(int argc, char** argv) {
+    
+    char* magic = new char[2]; 
+    int x = 0;
+    int y = 0;
+    int pixel = 3;
+    unsigned char* array = nullptr;
+    
+    readPPM(magic, &x, &y, &pixel, &array);
+    toJPEG("test.jpg", array, x, y, 90);
+
+    delete [] array;
+    array = nullptr;
+
+}
